15171ecba0f2bcbb3c3a74b24e9646cc0ba2c1a1
Guy Morita <guymorita@gmail.com>
2017-03-23 19:40:16 +0700

    add ListView and fetch

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,14 +1,43 @@
 
 import React, { Component } from 'react';
 import {
+  ListView,
   Text,
   View
 } from 'react-native';
 
-export default class Home extends Component {
+import { connect } from 'react-redux'
+
+class Home extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+      ds
+    };
+  }
+
+  componentDidMount() {
+    console.log('fetching')
+    return fetch(`https://api.github.com/search/repositories?q=topic:ruby+topic:rails`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          dataSource: this.state.ds.cloneWithRows(json.items)
+        })
+      })
+  }
+
   render() {
     return (
-      <View><Text>Home</Text></View>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData.name}</Text>}
+      />
     );
   }
 }
+
+
+export default connect()(Home)
