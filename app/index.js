a952b13382a3f929236dd8954b33fc0d92d3b5dd
Guy Morita <guymorita@gmail.com>
2017-03-23 19:22:34 +0700

    add redux and reducer

--- app/index.js
+++ app/index.js
@@ -6,6 +6,12 @@ import {
   View
 } from 'react-native';
 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { reducer } from './reducers/reducer'
+
+const store = createStore(reducer)
+
 export default class App extends Component {
   render() {
     const routes = [
@@ -23,13 +29,15 @@ export default class App extends Component {
       }
     ]
     return (
-      <Navigator
-        initialRoute={routes[0]}
-        initialRouteStack={routes}
-        renderScene={(route, navigator) =>
-          <View><Text>Hi</Text></View>
-        }
-      />
+      <Provider store={store}>
+        <Navigator
+          initialRoute={routes[0]}
+          initialRouteStack={routes}
+          renderScene={(route, navigator) =>
+            <View><Text>Hi</Text></View>
+          }
+        />
+      </Provider>
     );
   }
 }
